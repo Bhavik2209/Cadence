0caede6b5bba9b2dee82af17c47659008584b3a8
Bhavik2209 <bhavikrohit22@gmail.com>
2024-06-22 13:10:21 +0530

    del

--- testproject/djangoproject/posts/urls.py
+++
@@ -1,7 +0,0 @@
-# myapp/urls.py
-from django.urls import path
-from .views import process_data_view
-
-urlpatterns = [
-    path('processdata', process_data_view , name='process_data'),
-]
--- testproject/djangoproject/posts/utils.py
+++
@@ -1,5 +0,0 @@
-# myapp/utils.py
-def process_data(input_data):
-    # Perform some data processing
-    processed_data = input_data[::-1]  # Just reversing the string for demonstration
-    return processed_data
