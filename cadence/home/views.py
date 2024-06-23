6ee550b7b9a99e50bff6068e898e4ddfafca575c
Devam <satasiyadevam@gmail.com>
2024-06-23 16:07:49 +0530

    product page

--- cadence/home/views.py
+++ cadence/home/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render,redirect
 from .forms import UserRegForm,UserLoginForm
 
 import pyrebase
+
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 config = {
-    "apiKey": "${}",
+    "apiKey": "${my_api_ky}",
   "authDomain": "test-6ef0b.firebaseapp.com",
   "databaseURL": "https://test-6ef0b-default-rtdb.asia-southeast1.firebasedatabase.app",
   "projectId": "test-6ef0b",
@@ -40,7 +41,7 @@ def signup(request):
                 uid = user['localId']
                 request.session['uid'] = uid
                 messages.success(request, 'Account created successfully.')
-                return redirect('login')  # Redirect to login page after successful signup
+                return redirect('login') # Redirect to login page after successful signup
             except Exception as e:
                 messages.error(request, f'Error creating account: {str(e)}')
                 return render(request, "signup.html", {'form': form})
@@ -51,24 +52,24 @@ def signup(request):
 
     return render(request, 'signup.html', {'form': form})
 
-def login(request):
+def user_login(request):
     if request.method == 'POST':
         form = UserLoginForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
             try:
-        # if there is no error then signin the user with given email and password
-                user=authe.sign_in_with_email_and_password(email,password)
+                user = authe.sign_in_with_email_and_password(email, password)
+                session_id = user['idToken']
+                request.session['uid'] = str(session_id)
+                return redirect("index")  # Redirect to home page after successful login
             except:
-                message="Invalid Credentials!!Please ChecK your Data"
-                return render(request,"Login.html",{"message":message})
-            session_id=user['idToken']
-            request.session['uid']=str(session_id)
-            return render(request,"index.html")
+                message = "Invalid Credentials!! Please check your data."
+                return render(request, "login.html", {"message": message, "form": form})
         else:
             messages.error(request, 'Invalid form data')
     else:
         form = UserLoginForm()  # Create a new form instance for GET requests
-    
+
     return render(request, 'login.html', {'form': form})
+
