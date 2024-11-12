1bab28e9b4f0386ef2a9870070d53f7a2e29e68c
Bhavik2209 <bhavikrohit22@gmail.com>
2024-11-12 20:26:19 +0530

    added files

--- cadence/cadence/urls.py
+++ cadence/cadence/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('commit/',include('home.urls')),
+    path('commit/', include('home.urls')),
+    path('', lambda request: redirect('commit/'), name='root_redirect'),
 ]
